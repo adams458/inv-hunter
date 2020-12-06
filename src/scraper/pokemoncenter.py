e0c725386365c7ed303d8c1d9ca5d51368977c3c
Nicholas Adams <adams458@live.marshall.edu>
2020-12-06 00:48:51 -0500

    Update poke scraper

--- src/scraper/pokemoncenter.py
+++ src/scraper/pokemoncenter.py
@@ -1,5 +1,3 @@
-import logging
-
 from scraper.common import ScrapeResult, Scraper, ScraperFactory
 
 
@@ -9,23 +7,23 @@ class PokemonCenterScrapeResult(ScrapeResult):
         alert_content = ''
 
         # get name of product
-        tag = self.soup.body.find('h1', class_='jsx-1022525614 product-title')
+        tag = self.soup.p('h1', class_='jsx-1022525614 product-title')
         if tag:
             alert_content += tag.text.strip() + '\n'
         else:
-            logging.warning(f'missing title: {self.url}')
+            self.logger.warning(f'missing title: {self.url}')
 
         # get listed price
-        tag = self.soup.body.select_one('p', class_='jsx-1022525614 product-price-value')
+        tag = self.soup.title.string('p', class_='jsx-1022525614 product-price-value')
         price_str = self.set_price(tag)
         if price_str:
             alert_subject = f'In Stock for {price_str}'
         else:
-            logging.warning(f'missing price: {self.url}')
+            self.logger.warning(f'missing price: {self.url}')
 
         # check for add to cart button
-        tag = self.soup.body.find('div', class_='jsx-1839038600 product-add btn btn-secondary')
-        if tag and 'Add to Cart' in tag.text.lower():
+        tag = self.soup.p(class_='jsx-1839038600 product-add btn btn-secondary')
+        if tag and 'add to cart' in tag.text.lower():
             self.alert_subject = alert_subject
             self.alert_content = f'{alert_content.strip()}\n{self.url}'
 
@@ -34,16 +32,12 @@ class PokemonCenterScrapeResult(ScrapeResult):
 class PokemonCenterScraper(Scraper):
     @staticmethod
     def get_domain():
-        return 'pokemoncenter'
+        return 'p'
+
+    @staticmethod
+    def get_driver_type():
+        return 'requests'
 
     @staticmethod
     def get_result_type():
         return PokemonCenterScrapeResult
-
-    @staticmethod
-    def generate_short_name(url):
-        parts = [i for i in url.path.split('/') if i]
-        if parts:
-            return parts[1]
-            
-time.sleep(2)
